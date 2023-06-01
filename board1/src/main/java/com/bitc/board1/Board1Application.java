5af5fc49e0ce732bbe7607c2d40d20fc40e0875f
lecture-bisper <bisper@outlook.kr>
2023-06-01 15:53:41 +0900

    1. 스프링부트 프로젝트 board1 생성

---
+++ board1/src/main/java/com/bitc/board1/Board1Application.java
@@ -0,0 +1,13 @@
+package com.bitc.board1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Board1Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Board1Application.class, args);
+  }
+
+}
---
+++ board1/src/test/java/com/bitc/board1/Board1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitc.board1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Board1ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
