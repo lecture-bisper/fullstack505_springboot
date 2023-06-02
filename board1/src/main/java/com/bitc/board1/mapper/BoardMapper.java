29dbe78d9c7add3d83a1e08d859b96589be63916
lecture-bisper <bisper@outlook.kr>
2023-06-02 16:51:43 +0900

    1. 데이터 베이스 요청 처리를 위한 Mapper 인터페이스 및 xml 파일에 selectBoardDetail, insertBoard, updateBoard, deleteBoard, updateHitCount 메소드 추가 2. xml 파일에 SQL 문 추가

--- board1/src/main/java/com/bitc/board1/mapper/BoardMapper.java
+++ board1/src/main/java/com/bitc/board1/mapper/BoardMapper.java
@@ -2,6 +2,7 @@ package com.bitc.board1.mapper;
 
 import com.bitc.board1.dto.BoardDto;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -10,5 +11,33 @@ import java.util.List;
 //해당 인터페이스의 추상 메소드와 xml 파일의 태그명을 1:1로 연동해주는 어노테이션
 @Mapper
 public interface BoardMapper {
+//  게시판 글 목록
   List<BoardDto> selectBoardList() throws Exception;
+
+// 게시판 상세 글 확인
+  BoardDto selectBoardDetail(int boardIdx) throws Exception;
+
+//  게시판 글 등록
+  void insertBoard(BoardDto board) throws Exception;
+
+//  게시판 글 수정
+  void updateBoard(BoardDto board) throws Exception;
+
+//  @Param : 연결된 xml 파일에서 사용할 매개변수명을 새로 지정하는 어노테이션
+//  게시판 글 삭제
+  void deleteBoard(@Param("idx") int boardIdx) throws Exception;
+
+//  게시물 조회수 업데이트
+  void updateHitCount(int boardIdx) throws Exception;
 }
+
+
+
+
+
+
+
+
+
+
+
