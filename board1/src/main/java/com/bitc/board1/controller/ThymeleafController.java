3a9df5f1d98161fca6f5bdaddcebaefdfabafca1
lecture-bisper <bisper@outlook.kr>
2023-06-07 16:45:11 +0900

    1. 뷰 페이지 사용을 위한 메소드 추가 2. 선택 변수 사용 3. 리터널 문자열 사용, 숫자 사용, 논리형 사용, 리터럴 대체 || 사용 4. 연산자 사용 (산술 연산, 비교 연산) 5. 조건문 사용 (삼항연산자, if, unless, switch) 6. 반복문 사용, 반복문 상태 변수 사용 7. 태그 속성 값 설정하기 (attr, attrappend, attrprepend, th:html속성명) 8. 고정값 논리 속성 설정하기

--- board1/src/main/java/com/bitc/board1/controller/ThymeleafController.java
+++ board1/src/main/java/com/bitc/board1/controller/ThymeleafController.java
@@ -1,8 +1,12 @@
 package com.bitc.board1.controller;
 
+import com.bitc.board1.dto.BoardDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/thymeleaf")
@@ -24,4 +28,87 @@ public class ThymeleafController {
     return mv;
   }
 
+  @RequestMapping("/test2")
+  public ModelAndView test2() throws Exception {
+    ModelAndView mv = new ModelAndView("/thymeleaf/test2");
+
+    BoardDto board1 = new BoardDto();
+    board1.setBoardIdx(10);
+    board1.setTitle("테스트 제목");
+    board1.setContents("테스트 내용");
+    board1.setCreateId("tester1");
+    board1.setCreateDt("2023-06-07 09:29:00");
+    board1.setHitCnt(10);
+
+    mv.addObject("board", board1);
+
+    mv.addObject("str1", "첫번째");
+    mv.addObject("str2", "두번째");
+    mv.addObject("str3", null);
+
+    mv.addObject("num1", 100);
+    mv.addObject("num2", 3);
+    mv.addObject("num3", 11);
+
+    mv.addObject("level", "B");
+
+    String[] strArray = {"첫번째", "두번째", "세번째", "네번째", "다섯번째"};
+    List<String> strList = new ArrayList<String>();
+    strList.add("첫번째");
+    strList.add("두번째");
+    strList.add("세번째");
+    strList.add("네번째");
+    strList.add("다섯번째");
+
+    mv.addObject("itemArray", strArray);
+    mv.addObject("itemList", strList);
+
+    BoardDto board2 = new BoardDto();
+    board2.setBoardIdx(11);
+    board2.setTitle("테스트 제목2");
+    board2.setContents("테스트 내용2");
+    board2.setCreateId("tester2");
+    board2.setCreateDt("2023-06-07 12:07:00");
+    board2.setHitCnt(10);
+
+    List<BoardDto> boardList = new ArrayList<BoardDto>();
+    boardList.add(board1);
+    boardList.add(board2);
+
+    List<BoardDto> boardList2 = new ArrayList<>();
+
+    for (int i = 0; i < 10; i++) {
+      BoardDto board = new BoardDto();
+      board.setBoardIdx(i + 10);
+      board.setTitle("제목 " + (i + 10));
+
+      boardList2.add(board);
+    }
+
+    mv.addObject("boardList2", boardList2);
+
+    mv.addObject("boardList", boardList);
+
+    mv.addObject("chkVal", true);
+    mv.addObject("mulVal", true);
+    mv.addObject("selVal", true);
+
+    return mv;
+  }
+
+  @RequestMapping("/test3")
+  public String test3() throws Exception {
+    return "/thymeleaf/test3";
+  }
+
+  @RequestMapping("/test4")
+  public String test4() throws Exception {
+    return "/thymeleaf/test4";
+  }
 }
+
+
+
+
+
+
