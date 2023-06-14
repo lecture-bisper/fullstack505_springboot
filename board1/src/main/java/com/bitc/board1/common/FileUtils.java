20f9bb07eef9f2debff8b3bcd24c8ad719369332
lecture-bisper <bisper@outlook.kr>
2023-06-14 09:39:33 +0900

    1. board1 프로젝트의 스프링부트 버전 3.1.0으로 다시 변경 2. board1 프로젝트의 mybatis 버전 3.0.2으로 다시 변경 3. WebMvcConfiguration에서 CommonsMultipartResolver 대신 StandardServletMultipartResolver 을 사용한 파일 업로드 설정 4. FileUploads 클래스에서 파일 저장위치를 실제 경로로 변경함

--- board1/src/main/java/com/bitc/board1/common/FileUtils.java
+++ board1/src/main/java/com/bitc/board1/common/FileUtils.java
@@ -37,7 +37,9 @@ public class FileUtils {
     ZonedDateTime current = ZonedDateTime.now();
 
 //    파일 저장 경로 생성, 현재 타임존 기준 시간을 패턴에 맞게 설정하여 출력
-    String path = "images/" + current.format(format);
+//    String path = "images/" + current.format(format);
+//    현재 StandardServletMultipartResolver 사용 시 전체 경로가 아니면 파일 복사 시 오류가 발생함
+    String path = "C:/upload/" + current.format(format);
 
 //    자바의 File 클래스 객체 생성, 위에서 생성한 경로로 생성
     File file = new File(path);
--- board1/src/main/java/com/bitc/board1/configuration/WebMvcConfiguration.java
+++ board1/src/main/java/com/bitc/board1/configuration/WebMvcConfiguration.java
@@ -1,8 +1,13 @@
 package com.bitc.board1.configuration;
 
+import jakarta.servlet.MultipartConfigElement;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+//스프링6 (스프링부트3)에서 CommonsMultipartResolver 더이상 사용하지 않음, StandardServletMultipartResolver로 변경됨
+//import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.util.unit.DataSize;
+import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,14 +16,42 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
 
+//  스프링6 (스프링부트3)에서 CommonsMultipartResolver를 사용할 수 없기 때문에 예전 방식으로 설정이 불가능함
+//  @Bean
+//  public CommonsMultipartResolver multipartResolver() {
+////    apache commons-fileupload 설정 객체 생성
+//    CommonsMultipartResolver resolver = new CommonsMultipartResolver();
+////    사용할 문자셋 설정
+//    resolver.setDefaultEncoding("UTF-8");
+////    업로드할 파일의 최대 크기 설정(byte 단위),
+//    resolver.setMaxUploadSizePerFile(10 * 1024 * 1024);
+//    return resolver;
+//  }
+
+//  StandardServletMultipartResolver를 사용한 업로드 파일 설정
   @Bean
-  public CommonsMultipartResolver multipartResolver() {
-//    apache commons-fileupload 설정 객체 생성
-    CommonsMultipartResolver resolver = new CommonsMultipartResolver();
-//    사용할 문자셋 설정
-    resolver.setDefaultEncoding("UTF-8");
-//    업로드할 파일의 최대 크기 설정(byte 단위),
-    resolver.setMaxUploadSizePerFile(10 * 1024 * 1024);
-    return resolver;
+  public MultipartResolver multipartResolver() {
+    return new StandardServletMultipartResolver();
+  }
+
+  @Bean
+  public MultipartConfigElement multipartConfigElement() {
+    MultipartConfigFactory factory = new MultipartConfigFactory();
+//    업로드 파일의 크기 설정
+    factory.setMaxRequestSize(DataSize.ofBytes(5 * 1024 * 1024));
+    factory.setMaxFileSize(DataSize.ofBytes(5 * 1024 * 1024));
+
+    return factory.createMultipartConfig();
   }
 }
+
+
+
+
+
+
+
+
+
+
+
